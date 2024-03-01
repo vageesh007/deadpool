18cd867606c3e06d3972bfd9001e482f3c52b1d8
vageesh007 <159996291+vageesh007@users.noreply.github.com>
2024-03-01 07:46:46 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,32 @@
+let signupbtn = document.querySelector('.signupbtn');
+let signinbtn = document.querySelector('.signinbtn');
+let namefield = document.querySelector('.namefield');
+let title = document.querySelector('.title');
+let underline = document.querySelector('.underline');
+let text = document.querySelector('.text');
+
+
+signinbtn.addEventListener('click',()=>{
+    namefield.style.maxHeight = "0";
+    title.innerHTML = 'sign in';
+    text.innerHTML = 'lost password';
+    signupbtn.classList.add('disable');
+    signinbtn.classList.remove('disable');
+    underline.style.transform = 'translateX(35px)';
+
+});
+
+
+signupbtn.addEventListener('click',()=>{
+    namefield.style.maxHeight = "60px";
+    title.innerHTML = 'sign up';
+    text.innerHTML= 'use your brain again ';
+    signupbtn.classList.remove('disable');
+    signinbtn.classList.add('disable');
+    underline.style.transform = 'translateX(0px)';
+
+});
+
+
+
+
